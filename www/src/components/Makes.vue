72fd3ea7a3668f5f25460ee59b225930df36a42a
grzesiekmq <grzesiekfuta@gmail.com>
2019-12-16 11:21:33 +0100

    partials to vue components

---
+++ www/src/components/Makes.vue
@@ -0,0 +1,89 @@
+<template>
+        <div class="page-content">
+
+            <router-link to="tracks" class="tile-medium mt-1 p-1 text-center text-upper va-middle bg-white border bd-orange fg-orange">
+                    <h2>
+        
+                        back
+                    </h2>
+            </router-link>
+    <h2 class="cars-title fg-orange text-center mt-14-minus">Make selection</h2>
+    <div class="block cars">
+        <div class="row" @click="makeHandler">
+
+            
+            <router-link :to="{name:'models', params: {make: make}}" v-for="make of makes" :key="make" class="col-100 tile-small m-2 p-1 va-middle bg-orange outline ol-white fg-black">
+
+                   <h4>
+
+                       {{make}}
+                   </h4>
+            </router-link>
+            
+            
+        </div>
+    </div>
+    </div>
+</template>
+<script>
+import bus from '../eventBus'
+const makes = [
+                        'adi',
+                        'akura',
+                        'alfa',
+                        'aston',
+                        'bentle',
+                        'bmv',
+                        'bugati',
+                        'cadilac',
+                        'chonda',
+                        'citron',
+                        'dodg',
+                        'ferari',
+                        'fort',
+                        'henesey',
+                        'hevrolet',
+                        'holdem',
+                        'hrysler',
+                        'jagur',
+                        'konigseg',
+                        'lambo',
+                        'lotuz',
+                        'luxus',
+                        'maklaren',
+                        'masda',
+                        'maybah',
+                        'mazerati',
+                        'merc',
+                        'mitsushi',
+                        'nisan',
+                        'opl',
+                        'other',
+                        'pahani',
+                        'pontiak',
+                        'porshe',
+                        'rols-roys',
+                        'rufe',
+                        'sab',
+                        'salen',
+                        'shelbi',
+                        'tezla',
+                        'tojota',
+                        'twr',
+                        'vendeta',
+                        'vw',
+                        'zenwo',
+                        'zubaru'
+                    ]
+export default {
+    data(){
+        return {makes}
+    },
+    methods:{
+        makeHandler(event){
+            bus.$emit('clickedMake', event.target.textContent.trim())
+        }
+    }
+    
+}
+</script>
---
+++ www/src/components/Models.vue
@@ -0,0 +1,265 @@
+<template>
+  <div class="page-content">
+    <router-link
+      to="makes"
+      class="tile-medium mt-1 p-1 text-center text-upper va-middle bg-white border bd-orange fg-orange"
+    >
+      <h2>back</h2>
+    </router-link>
+    <h2 class="cars-title fg-orange text-center mt-14-minus">Model selection</h2>
+    <button
+      class="select-car tile-small place-right mt-17-minus p-1 text-center bg-orange border bd-orange outline ol-white fg-black"
+    >Select</button>
+
+    <div class="info"></div>
+
+    <!-- <div class="block models"> -->
+    <!-- <div class="row"> -->
+
+    <ul class="inline-list" @click="modelHandler" v-for="models in filteredModels" :key="models.id">
+      <li v-for="model in models" :key="model">
+        <button
+          class="col-100 tile-small m-2 p-1 va-middle bg-orange border bd-orange outline ol-white fg-black"
+        >
+          <h4 class="model-name">{{model}}</h4>
+        </button>
+      </li>
+    </ul>
+
+    <!-- </div> -->
+    <!-- </div> -->
+  </div>
+</template>
+                    
+<script>
+import axios from "axios";
+import bus from "../eventBus";
+
+const json = "www/data/carProps.json";
+
+const cars = [
+  {
+    name: "adi",
+    models: [
+      "a8",
+      "avus",
+      "r18",
+      "r8 fsi",
+      "r8 le mans",
+      "rs7",
+      "rsq",
+      "tt clubsport"
+    ]
+  },
+
+  { name: "akura", models: ["nzx", "xrx", "NZX GT3"] },
+
+  { name: "alfa", models: ["8c spider", "zagato tz3 stradale"] },
+
+  {
+    name: "aston",
+    models: [
+      "DBR9",
+      "dbs volante",
+      "one 77",
+      "rapide",
+      "v12 vanquish",
+      "vanquish",
+      "vantage",
+      "vulcan"
+    ]
+  },
+
+  { name: "bentle", models: ["continental gt supersports"] },
+
+  {
+    name: "bmv",
+    models: [
+      "7",
+      "8 series concept",
+      "i8",
+      "m3 e92",
+      "M3 GTR",
+      "m3",
+      "m5 tuning",
+      "m6"
+    ]
+  },
+
+  { name: "bugati", models: ["chiron", "veyron"] },
+
+  { name: "cadilac", models: ["cien", "cts"] },
+
+  { name: "chonda", models: ["NZX TAKATA DOME", "s2000 tuned"] },
+
+  { name: "citron", models: ["citron gt"] },
+
+  {
+    name: "dodg",
+    models: ["viper gts", "viper zrt", "viper", "zrt challenger"]
+  },
+
+  {
+    name: "ferari",
+    models: [
+      "430",
+      "458 italia",
+      "599",
+      "812 superfast",
+      "f12",
+      "la ferari",
+      "modena",
+      "testarosa"
+    ]
+  },
+
+  { name: "fort", models: ["gt 90", "gt", "mustang salen", "mustang"] },
+
+  { name: "henesey", models: ["venom gt"] },
+
+  {
+    name: "hevrolet",
+    models: ["camaro old", "camaro", "corvete concept", "corvette C7"]
+  },
+
+  { name: "holdem", models: ["monaro"] },
+
+  { name: "hrysler", models: ["300c", "crossfire", "ME"] },
+
+  { name: "jagur", models: ["c-x75", "f type", "xkr"] },
+
+  { name: "konigseg", models: ["agera", "ccx", "one1"] },
+
+  {
+    name: "lambo",
+    models: [
+      "asterion",
+      "aventador",
+      "diablo sv",
+      "gallardo",
+      "huracan",
+      "murcielago",
+      "reventon",
+      "urus",
+      "veneno"
+    ]
+  },
+
+  { name: "lotuz", models: ["evora", "exige s"] },
+
+  { name: "luxus", models: ["gs", "lfa"] },
+
+  { name: "maklaren", models: ["576 gt", "f1", "GT", "mp4 12c", "mp4", "p1"] },
+
+  { name: "masda", models: ["MX 5", "mx5 2016", "RX 7", "RX 8"] },
+
+  { name: "maybah", models: ["exelero"] },
+
+  { name: "mazerati", models: ["G Turismo", "mc12", "quatroporte", "spyder"] },
+  {
+    name: "merc",
+    models: [
+      "amge gts",
+      "cl",
+      "clk dtm",
+      "clk gtr",
+      "slr maklaren",
+      "sls amge",
+      "sls"
+    ]
+  },
+
+  { name: "mitsushi", models: ["3000gt", "eclipse", "lancer evolution x"] },
+
+  { name: "nisan", models: ["370z", "gtr nizmo", "R35 GTR", "skyline gtr"] },
+
+  { name: "opl", models: ["speedster"] },
+
+  {
+    name: "other",
+    models: [
+      "asterisk",
+      "concept car 2009",
+      "concept car 7",
+      "concept car5",
+      "exotic car",
+      "race car",
+      "vm x1",
+      "wizard gt"
+    ]
+  },
+
+  { name: "pahani", models: ["huayra", "zonda f"] },
+
+  { name: "pontiak", models: ["firebird", "gto"] },
+
+  {
+    name: "porshe",
+    models: [
+      "911 turbo",
+      "918 r type",
+      "996",
+      "997 gt3",
+      "boxster s",
+      "boxster",
+      "carrera gt",
+      "cayman 2017",
+      "panamera turbo"
+    ]
+  },
+
+  { name: "rols-roys", models: ["phantom"] },
+
+  { name: "rufe", models: ["rt 12s"] },
+
+  { name: "sab", models: ["aero x"] },
+
+  { name: "salen", models: ["s7"] },
+
+  { name: "shelbi", models: ["cobra"] },
+
+  { name: "tezla", models: ["models s"] },
+
+  { name: "tojota", models: ["soarer"] },
+
+  { name: "twr", models: ["sagaris"] },
+
+  { name: "vendeta", models: ["gtr 800"] },
+
+  { name: "vw", models: ["ego"] },
+
+  { name: "zenwo", models: ["st1"] },
+
+  { name: "zubaru", models: ["b11s", "impreza"] }
+];
+
+export default {
+  data() {
+    return { cars, clickedMake: "" };
+  },
+  methods: {
+    getCars() {
+      return axios.get(json).then(response => console.log(response.data));
+    },
+    modelHandler(event) {
+      bus.$emit("clickedModel", event.target.textContent.trim());
+    },
+    onMakeClicked(clickedMake) {
+      this.clickedMake = clickedMake;
+    }
+  },
+  created() {
+    bus.$on("clickedMake", this.onMakeClicked);
+  },
+  destroyed() {
+    bus.$off("clickedMake", this.onMakeClicked);
+  },
+  computed: {
+    filteredModels() {
+      return cars
+        .filter(make => make.name === this.clickedMake)
+        .map(make => make.models);
+    }
+  }
+};
+</script>
